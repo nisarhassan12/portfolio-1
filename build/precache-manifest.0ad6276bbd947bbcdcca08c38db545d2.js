5e12740308012fdafbf23665cba3c7e48479d742
billal <proffesergio@gmail.com>
2020-03-15 01:22:09 +0600

    up and running, modified title

---
+++ build/precache-manifest.0ad6276bbd947bbcdcca08c38db545d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f131f64c6b2282d40d8cc188c575a99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d14090c437004d687665",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "ff02f3941061c02d4c1e",
+    "url": "/static/js/2.c8744554.chunk.js"
+  },
+  {
+    "revision": "d14090c437004d687665",
+    "url": "/static/js/main.b90ec69d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ab50a9be7c8b1d9a600681f51ce4e77e",
+    "url": "/static/media/profile.ab50a9be.jpg"
+  }
+]);
